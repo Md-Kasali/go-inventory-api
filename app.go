3a2b75b3f94a3e295d8af50a4ed8b38eaed89772
Kasali <kasaliraphael123@gmail.com>
2024-09-18 20:06:42 +0530

    Updated Initialise method to get  database parameters from the main function

--- app.go
+++ app.go
@@ -17,7 +17,7 @@ type App struct {
 	Db     *sql.DB
 }
 
-func (app *App) Initialise() error {
+func (app *App) Initialise(DbUser string, DbPassword string, DbName string) error {
 	connectionString := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/%v", DbUser, DbPassword, DbName)
 	var err error
 	app.Db, err = sql.Open("mysql", connectionString)
@@ -91,7 +91,7 @@ func (app *App) createProduct(w http.ResponseWriter, r *http.Request) {
 		sendError(w, http.StatusInternalServerError, err.Error())
 	}
 
-	sendResponse(w, http.StatusOK, p)
+	sendResponse(w, http.StatusCreated, p)
 }
 
 func (app *App) updateProduct(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main() {
 
 	app := App{}
-	app.Initialise()
+	app.Initialise(DbUser, DbPassword, DbName)
 	app.Run("localhost:8000")
 }
